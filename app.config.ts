ca7f903aea64fc470c8c5f88bbd58e223503dfb4
Derpius <49565664+Derpius@users.noreply.github.com>
2025-12-28 22:37:20 +0000

    Configure MDX

--- app.config.ts
+++ app.config.ts
@@ -2,15 +2,24 @@ import { defineConfig } from "@solidjs/start/config";
 import type { PluginOption } from "vite";
 import solidStyled from "unplugin-solid-styled";
 
+/* @ts-ignore */
+import mdx from "@vinxi/plugin-mdx";
+
 export default defineConfig({
+  extensions: ["mdx", "md"],
   vite: {
     plugins: [
       solidStyled.vite({
         filter: {
           include: "src/**/*.tsx",
-          exclude: "node_modules/**/*.{ts,js}"
-        }
-      }) as PluginOption
-    ]
-  }
+          exclude: "node_modules/**/*.{ts,js}",
+        },
+      }) as PluginOption,
+      mdx.withImports({})({
+        jsx: true,
+        jsxImportSource: "solid-js",
+        providerImportSource: "solid-mdx",
+      }),
+    ],
+  },
 });
