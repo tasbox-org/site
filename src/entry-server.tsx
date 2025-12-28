e7b9d352ef0874422c0602cd304606e6738e6d73
Derpius <49565664+Derpius@users.noreply.github.com>
2025-12-28 22:27:45 +0000

    Add favicons

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,9 +6,15 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang="en">
         <head>
+          <title>TASBox</title>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" href="/img/favicon-96x96.png" sizes="96x96" />
+          <link rel="icon" type="image/svg+xml" href="/img/favicon.svg" />
+          <link rel="shortcut icon" href="/img/favicon.ico" />
+          <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" sizes="180x180" />
+          <meta name="apple-mobile-web-app-title" content="TASBox" />
+          <link rel="manifest" href="/site.webmanifest" />
           {assets}
         </head>
         <body>
