fe19bf35d6553fe03f5829d1a85a7294f41ba29a
Derpius <49565664+Derpius@users.noreply.github.com>
2026-03-01 21:55:19 +0000

    Move description and alt text to frontmatter

--- src/data/blog-posts.ts
+++ src/data/blog-posts.ts
@@ -3,6 +3,8 @@ import JsonPosts from "./blog-posts.json";
 interface BlogPost {
   slug: string;
   title: string;
+  description: string;
+  thumbnailAltText: string;
   authors: string[];
   tags: string[];
 }
