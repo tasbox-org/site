4cd56d3f1e4f83e65ff8cd9fbcc40d8c44306f28
Derpius <49565664+Derpius@users.noreply.github.com>
2026-03-05 22:08:57 +0000

    Add docs package

--- src/routes/docs/index.tsx
+++ src/routes/docs/index.tsx
@@ -1,10 +1,13 @@
 import { Title } from "@solidjs/meta";
 import { Component } from "solid-js";
+import { event } from "@tasbox-org/docs";
 
 const Docs: Component = () => (
   <main>
     <Title>TASBox Docs</Title>
-    <p>TODO</p>
+    <pre>
+      <code>{JSON.stringify(event, null, 2)}</code>
+    </pre>
   </main>
 );
 
