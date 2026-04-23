8ea2869842aeb5acc084e0e4e9c2699ea72f6cf2
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-23 21:38:39 +0100

    Switch to Biome

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,5 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
+// biome-ignore lint/style/noNonNullAssertion: Guaranteed to exist by SolidStart
 mount(() => <StartClient />, document.getElementById("app")!);
