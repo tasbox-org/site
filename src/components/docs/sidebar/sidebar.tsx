662ac76fbfbd4916cf857301fd27413d9e80a36d
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-25 18:48:18 +0100

    Add primary docs sidebar

---
+++ src/components/docs/sidebar/sidebar.tsx
@@ -0,0 +1,36 @@
+import { For } from "solid-js";
+
+export interface DocsSidebarItem {
+  title: string;
+  href: string;
+}
+
+export interface DocsSidebarSection {
+  title: string;
+  items: DocsSidebarItem[];
+}
+
+export interface DocsSidebarProps {
+  sections: DocsSidebarSection[];
+}
+
+export const DocsSidebar = (props: DocsSidebarProps) => (
+  <nav aria-label="Documentation">
+    <For each={props.sections}>
+      {(section) => (
+        <>
+          <h1>{section.title}</h1>
+          <ul>
+            <For each={section.items}>
+              {(item) => (
+                <li>
+                  <a href={item.href}>{item.title}</a>
+                </li>
+              )}
+            </For>
+          </ul>
+        </>
+      )}
+    </For>
+  </nav>
+);
---
+++ src/routes/docs.tsx
@@ -0,0 +1,24 @@
+import { allLibraries } from "@tasbox-org/docs";
+import type { ParentProps } from "solid-js";
+import { DocsSidebar } from "#components/docs/sidebar";
+import style from "./docs.module.css";
+
+const DocsLayout = (props: ParentProps) => (
+  <div class={style.container}>
+    <DocsSidebar
+      sections={[
+        {
+          title: "Guides",
+          items: [{ title: "TODO", href: "#" }],
+        },
+        {
+          title: "API",
+          items: allLibraries.map((library) => ({ title: library.name, href: `/docs/api/${library.name}` })),
+        },
+      ]}
+    />
+    {props.children}
+  </div>
+);
+
+export default DocsLayout;
