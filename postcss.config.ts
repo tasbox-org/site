935821b7126ed1e81a5021da0a069b7562fae259
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-25 16:54:53 +0100

    add light/dark postcss rule

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,10 +1,11 @@
 import autoprefixer from "autoprefixer";
+import postcssDarkThemeClass from "postcss-dark-theme-class";
 import type { Config } from "postcss-load-config";
 import postcssNested from "postcss-nested";
 
 const config: Config = {
   // @ts-expect-error
-  plugins: [autoprefixer, postcssNested],
+  plugins: [autoprefixer, postcssNested, postcssDarkThemeClass],
 };
 
 // @ts-expect-error
