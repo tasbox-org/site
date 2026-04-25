e1291bb473fc6357fb7c7ab2fb3cc4d9e0ae9ce5
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-25 16:03:21 +0100

    Revert "Marginally more progress"
    This reverts commit dba56e09d4b2bd4ad4a1cc9933246a8ab61b02b7.

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,9 @@
 import { createRouter } from "@tanstack/solid-router";
-import { ErrorBoundary } from "#components/common/error-boundary";
 import { NotFound } from "#components/common/not-found";
-import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () =>
   createRouter({
-    routeTree,
     defaultPreload: "intent",
     scrollRestoration: true,
-    defaultErrorComponent: ErrorBoundary,
-    defaultNotFoundComponent: () => <NotFound />,
+    defaultNotFoundComponent: NotFound,
   });
