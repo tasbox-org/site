4936e32a3c03c42578810e3a1bf8e42fc51881a8
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-26 15:34:33 +0100

    More main layout fixes

--- src/routes/docs/index.tsx
+++ src/routes/docs/index.tsx
@@ -1,13 +1,10 @@
 import { Title } from "@solidjs/meta";
-import { event } from "@tasbox-org/docs";
 import type { Component } from "solid-js";
 
 const Docs: Component = () => (
   <>
-    <Title>TASBox Docs</Title>
-    <pre>
-      <code>{JSON.stringify(event, null, 2)}</code>
-    </pre>
+    <Title>TASBox - Docs</Title>
+    <p>Documentation</p>
   </>
 );
 
