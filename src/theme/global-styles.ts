8b392a5e8c8f4baa74e856633af98cdc57822e6a
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-27 21:40:24 +0100

    Common styles

--- src/theme/global-styles.ts
+++ src/theme/global-styles.ts
@@ -1,6 +1,8 @@
+import "./css-reset.css";
+
 import "./prisma/gruvbox.css";
 import "./colours.css";
-import "./css-reset.css";
+import "./common.css";
 import "./fonts.css";
 import "./markdown.css";
 import "./spacing.css";
