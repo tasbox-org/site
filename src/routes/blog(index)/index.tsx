d6fb21550befb0b24b3d4ce1baaf241d2b2d26d3
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-28 22:48:14 +0100

    Add full metadata

--- src/routes/blog(index)/index.tsx
+++ src/routes/blog(index)/index.tsx
@@ -1,10 +1,15 @@
-import { Title } from "@solidjs/meta";
 import { type Component, For } from "solid-js";
+import { Metadata } from "#components/common/metadata";
 import { blogPosts } from "#data/blog-posts";
 
 const BlogList: Component = () => (
   <>
-    <Title>TASBox - Blog</Title>
+    <Metadata
+      type="website"
+      title="Blog"
+      description="TASBox progress updates, sneak peaks, technical deep dives and more."
+      url="/blog"
+    />
     <For each={blogPosts}>{(post) => <a href={`/blog/${post.slug}`}>{post.title}</a>}</For>
   </>
 );
