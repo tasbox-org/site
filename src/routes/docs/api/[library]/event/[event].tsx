dd879031db507bb8fc62085ac6e52cb1da33bd3b
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-28 20:48:12 +0100

    Implement events

--- src/routes/docs/api/[library]/event/[event].tsx
+++ src/routes/docs/api/[library]/event/[event].tsx
@@ -1,9 +1,22 @@
 import { useParams } from "@solidjs/router";
-import { For } from "solid-js";
+import type { Property } from "@tasbox-org/docs";
+import { For, Show } from "solid-js";
+import { CodeBlock } from "#components/common/code-block/code-block";
 import { Description } from "#components/docs/description";
+import { PropertyList } from "#components/docs/property-list";
 import { RealmHeading } from "#components/docs/realm-heading";
+import { SecondaryHeading } from "#components/docs/secondary-heading";
+import { renderType } from "#helpers/render-type";
 import { useDocEntries } from "#hooks/use-doc-entries";
 import type { PathParams } from "#types/path-params";
+
+const renderParameterList = (parameters: Property[]) => parameters.map((param) => param.name).join(", ");
+
+const renderReturnList = (returns: Property[]) =>
+  returns.length > 0 ? `: ${returns.map((ret) => renderType(ret.type)).join(", ")}` : "";
+
+const renderEventListenerSignature = (libraryName: string, name: string, parameters: Property[], returns: Property[]) =>
+  `${libraryName}.addEventListener("${name}", fn(${renderParameterList(parameters)}) end)${renderReturnList(returns)}`;
 
 const EventPage = () => {
   const params = useParams<PathParams["/api/[library]/event/[event]"]>();
@@ -19,6 +32,17 @@ const EventPage = () => {
         <div>
           <RealmHeading realms={event.realms} name={event.name} />
           <Description value={event.description} />
+          <CodeBlock language="moonjuice">
+            {renderEventListenerSignature(params.library, event.name, event.parameters, event.returns)}
+          </CodeBlock>
+          <Show when={event.parameters.length > 0}>
+            <SecondaryHeading>Parameters</SecondaryHeading>
+            <PropertyList properties={event.parameters} />
+          </Show>
+          <Show when={event.returns.length > 0}>
+            <SecondaryHeading>Returns</SecondaryHeading>
+            <PropertyList properties={event.returns} />
+          </Show>
         </div>
       )}
     </For>
