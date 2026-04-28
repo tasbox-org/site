2288472b09ed760d5ce0e8b60cdfe31b28a32dae
Derpius <49565664+Derpius@users.noreply.github.com>
2026-04-28 20:54:12 +0100

    Implement functions

--- src/routes/docs/api/[library]/function/[function].tsx
+++ src/routes/docs/api/[library]/function/[function].tsx
@@ -1,9 +1,25 @@
 import { useParams } from "@solidjs/router";
-import { For } from "solid-js";
+import type { Property } from "@tasbox-org/docs";
+import { For, Show } from "solid-js";
+import { CodeBlock } from "#components/common/code-block/code-block";
 import { Description } from "#components/docs/description";
+import { PropertyList } from "#components/docs/property-list";
 import { RealmHeading } from "#components/docs/realm-heading";
-import type { PathParams } from "#types/path-params";
+import { SecondaryHeading } from "#components/docs/secondary-heading";
+import { renderType } from "#helpers/render-type";
 import { useDocEntries } from "#hooks/use-doc-entries";
+import type { PathParams } from "#types/path-params";
+
+const renderParameterSignature = (parameters: Property[]) =>
+  parameters
+    .map((parameter) => `${parameter.name}${parameter.optional ? "?" : ""}: ${renderType(parameter.type)}`)
+    .join(", ");
+
+const renderReturnSignature = (returns: Property[]) =>
+  returns.map((value) => `${renderType(value.type)}${value.optional ? "?" : ""}`).join(", ");
+
+const renderFunctionSignature = (name: string, parameters: Property[], returns: Property[]) =>
+  `${name}(${parameters.length > 0 ? renderParameterSignature(parameters) : ""})${returns.length > 0 ? `: ${renderReturnSignature(returns)}` : ""}`;
 
 const FunctionPage = () => {
   const params = useParams<PathParams["/api/[library]/function/[function]"]>();
@@ -19,6 +35,17 @@ const FunctionPage = () => {
         <div>
           <RealmHeading realms={func.realms} name={func.name} />
           <Description value={func.description} />
+          <CodeBlock language="moonjuice">
+            {renderFunctionSignature(func.name, func.parameters, func.returns)}
+          </CodeBlock>
+          <Show when={func.parameters.length > 0}>
+            <SecondaryHeading>Parameters</SecondaryHeading>
+            <PropertyList properties={func.parameters} />
+          </Show>
+          <Show when={func.returns.length > 0}>
+            <SecondaryHeading>Returns</SecondaryHeading>
+            <PropertyList properties={func.returns} />
+          </Show>
         </div>
       )}
     </For>
