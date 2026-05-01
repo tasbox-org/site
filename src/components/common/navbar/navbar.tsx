fcdaedebe44908c535d0144363a18ea877cda87c
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-02 00:07:42 +0100

    Make light dark toggle functional

--- src/components/common/navbar/navbar.tsx
+++ src/components/common/navbar/navbar.tsx
@@ -1,9 +1,21 @@
-import type { Component } from "solid-js";
+import { type Component, createEffect, createSignal } from "solid-js";
+import { isServer } from "solid-js/web";
 import { Button, ButtonNavLink } from "#components/common/button";
 import styles from "./navbar.module.css";
 
 // TODO: Add skip link to main content
 export const Navbar: Component = () => {
+  const [themeClass, setThemeClass] = createSignal<"is-light" | "is-dark" | undefined>(undefined);
+
+  createEffect(() => {
+    if (isServer) {
+      return;
+    }
+
+    // biome-ignore lint/style/noNonNullAssertion: Page will always have root html
+    document.getElementsByTagName("html")[0]!.classList = themeClass() ?? "";
+  });
+
   return (
     <div class={styles.navbar}>
       <a href="/">
@@ -27,7 +39,12 @@ export const Navbar: Component = () => {
         </ul>
       </nav>
       <div>
-        <Button>Light/Dark</Button>
+        <Button class="light-mode-only" onClick={() => setThemeClass("is-dark")}>
+          Dark
+        </Button>
+        <Button class="dark-mode-only" onClick={() => setThemeClass("is-light")}>
+          Light
+        </Button>
       </div>
     </div>
   );
