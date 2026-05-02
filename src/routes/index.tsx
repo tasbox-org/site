d80affa14d3073f4aa8817ff3e1392dfa09fb0ba
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-02 13:15:47 +0100

    Touch ups

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,10 @@ export default function Home() {
     <>
       <Metadata
         type="website"
-        description="Next-generation physics sandbox game built by veterans of the genre. Make games and addons using our extensive modding API or just have some fun with friends. Forever FOSS."
+        description="Next-generation physics sandbox game built by veterans of the genre. Make games and addons using our extensive scripting API or just have some fun with friends. Forever FOSS."
         url="/"
       />
-      <div class={styles.hero}>
+      <section class={styles.hero}>
         <div class={styles.overlay}>
           <h1>
             <img src="/img/logo-dark.svg" alt="TASBox" class={styles.heroImage} />
@@ -18,8 +18,8 @@ export default function Home() {
           <div class={styles.heroText}>
             <p>Next-generation physics sandbox game built by veterans of the genre</p>
             <p>
-              Make games and addons using <a href="/docs/guides/introduction">our extensive modding API</a> or just have
-              some fun with friends
+              Make games and addons using <a href="/docs/guides/introduction">our extensive scripting API</a> or just
+              have some fun with friends
             </p>
             <p>
               <em>
@@ -28,7 +28,6 @@ export default function Home() {
                   href="https://en.wikipedia.org/wiki/Free_and_open-source_software"
                   target="_blank"
                   rel="noopener noreferrer"
-                  class={styles.heroLink}
                 >
                   FOSS.
                 </a>
@@ -55,7 +54,7 @@ export default function Home() {
             </ButtonLink>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 }
