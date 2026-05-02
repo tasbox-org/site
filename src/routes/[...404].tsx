62a49e9aa132c80ba5b8253c278f2eee4d418eb3
Derpius <49565664+Derpius@users.noreply.github.com>
2026-05-02 11:03:30 +0100

    Update 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,19 +1,19 @@
 import { Title } from "@solidjs/meta";
 import { HttpStatusCode } from "@solidjs/start";
 
-export default function NotFound() {
-  return (
-    <>
-      <Title>Not Found</Title>
-      <HttpStatusCode code={404} />
+const NotFound = () => (
+  <>
+    <Title>Not Found</Title>
+    <HttpStatusCode code={404} />
+    <div class="markdown-full">
       <h1>Page Not Found</h1>
       <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank" rel="noopener">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        We couldn't find a page at this address. Head to <a href="/">About</a> to read about what TASBox is,{" "}
+        <a href="/docs/guides/introduction">Docs</a> to learn how to make games & addons, or <a href="/blog">Blog</a>{" "}
+        for progress updates, technical deep dives and more.
       </p>
-    </>
-  );
-}
+    </div>
+  </>
+);
+
+export default NotFound;
